7910cc34a4d6ef1a443da2224c4cb97256ac583b
Marri5 <128259249+Marri5@users.noreply.github.com>
2024-12-13 10:30:08 +0100

    .

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const helloController = require("../controllers/helloController");
+const indexController = require('../controllers/indexController');
+
+router.get('/index', indexController.renderIndex);
 
 router.get("/helloworld", helloController.proxyToNginx);
 
