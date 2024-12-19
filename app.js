1298846c335fa8abbaa8bc3df78905d36cda2141
Marri5 <128259249+Marri5@users.noreply.github.com>
2024-12-19 11:11:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/api');
 
-mongoose.connect('mongodb://10.12.3.252:27017/oppgavemarathon', {
+// MongoDB Connection
+mongoose.connect('mongodb://10.12.[pool].252:27017/oppgavemarathon', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected successfully'))
 .catch(err => console.error('MongoDB connection error:', err));
 
+// Set view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Middleware
 app.use(express.json());
 
+// API routes
 app.use('/api', apiRoutes);
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
