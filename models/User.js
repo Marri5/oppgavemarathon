63f95bb7dc3c8df60b85574d17d14f35b496b68b
Marri5 <128259249+Marri5@users.noreply.github.com>
2024-12-19 11:14:34 +0100

    .

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+// Pre-save hook to hash passwords
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,46 @@ const router = express.Router();
 const helloController = require("../controllers/helloController");
 const indexController = require('../controllers/indexController');
 const mongoController = require('../controllers/mongoController');
+const User = require('../models/User');
 
 router.get('/index', indexController.renderIndex);
 
 router.get("/helloworld", helloController.proxyToNginx);
 
 router.get('/mongo-world', mongoController.renderMongoWorld);
+
+// Login Route
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username });
+        if (!user) return res.status(400).send('Invalid username or password');
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return res.status(400).send('Invalid username or password');
+
+        res.send('Login successful');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+// Registration Route
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userExists = await User.findOne({ username });
+        if (userExists) return res.status(400).send('User already exists');
+
+        const newUser = new User({ username, password });
+        await newUser.save();
+
+        res.send('User registered successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
+});
 
 module.exports = router;
